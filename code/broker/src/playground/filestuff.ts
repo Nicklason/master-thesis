fe706c7a30abf8991643212d1843ec4bf5463f78
Nicklason <Nicklason@users.noreply.github.com>
2024-05-25 12:36:38 +0200

    chore: add a test for appending messages to a file and reading them

---
+++ code/broker/src/playground/filestuff.ts
@@ -0,0 +1,58 @@
+import fs from "node:fs";
+import { MessageFactory } from "../messages/factory";
+import { Reader, Writer } from "protobufjs";
+
+// Write stream append to file
+
+const writeStream = fs.createWriteStream("output.txt", { flags: "a" });
+
+MessageFactory.setSource(1);
+
+const message = MessageFactory.ping();
+
+const encoded = message.encode();
+
+console.log("message length", encoded.length);
+
+const writer = Writer.create();
+writer.bytes(encoded);
+
+const buffer = writer.finish();
+
+writeStream.write(buffer);
+writeStream.end(() => {
+  console.log("Stream ended");
+
+  const readStream = fs.createReadStream("output.txt");
+
+  let chunky = Buffer.alloc(0);
+  let position = 0;
+
+  let length: number | undefined = undefined;
+
+  readStream.on("data", (chunk) => {
+    if (!Buffer.isBuffer(chunk)) {
+      return;
+    }
+
+    chunky = Buffer.concat([chunky.subarray(position), chunk]);
+
+    const reader = Reader.create(chunky);
+    reader.pos = position;
+
+    while (chunky.length - reader.pos >= 8) {
+      if (length === undefined) {
+        length = reader.uint32();
+        // Reset position
+        reader.pos = position;
+      }
+
+      if (chunky.length >= length) {
+        const message = reader.bytes();
+        console.log("message", message);
+        position = reader.pos;
+        length = undefined;
+      }
+    }
+  });
+});
